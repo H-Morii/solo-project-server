10cf3dc6dd875496653645f42a5ce3cd764d7df5
H-morii <haruki.mori21@gmail.com>
2024-02-21 11:23:43 +0900

    Fixed the script

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const knex = require("./db/knex");
-
 dotenv.config();
-
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
